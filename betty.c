29cc6fd633437cc51baaa8884d68c2ef8efdc722
RichPhilip <onuohaphilip444@gmail.com>
2023-05-25 20:16:21 +0000

    simple_shell.c

---
+++ betty.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define MAX_VALUE 100
+
+/**
+ * add_numbers - Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The sum of @a and @b
+ */
+int add_numbers(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int x = 10;
+	int y = 20;
+	int sum = add_numbers(x, y);
+
+	/* Check if the sum exceeds the maximum value */
+	if (sum > MAX_VALUE)
+	{
+
+		puts("The sum exceeds the maximum value.");
+	}
+	else
+	{
+		/* Print the value of the sum */
+		puts("The sum is within the allowed range.");
+	}
+
+	return (0);
+}
